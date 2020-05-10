1ed6123ddffc6dad0073cbff656b255371b4dcd9
Baboo <1143095390@qq.com>
2020-05-10 19:35:35 +0800

    city_lamp

---
+++ simulation_data_acquisition/src/main/java/cn/yifeng/simulation_data_acquisition/SimulationDataAcquisitionApplication.java
@@ -0,0 +1,13 @@
+package cn.yifeng.simulation_data_acquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationDataAcquisitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulationDataAcquisitionApplication.class, args);
+    }
+
+}
---
+++ simulation_data_acquisition/src/test/java/cn/yifeng/simulation_data_acquisition/SimulationDataAcquisitionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yifeng.simulation_data_acquisition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationDataAcquisitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
